2971d63efa33cb351f4f9f6c908a4a91b749b636
juliarinaa <rina.julianur@gmail.com>
2024-12-09 12:35:48 +0700

    hapusin yang gak perlu

--- Project/Launcher.cpp
+++ Project/Launcher.cpp
@@ -1,21 +1,6 @@
 #include "Setting.h"
 #include "Game.h"
 
-#include "Lesson01_BgColor.h"
-#include "Lesson02_Lerp.h"
-#include "Lesson03_Animation.h"
-#include "Lesson04_Movement.h"
-#include "Lesson05_Input.h"
-#include "Lesson06_ObjectPool.h"
-#include "Lesson07_GUI.h"
-#include "Lesson08_Rotation.h"
-#include "Lesson09_Bullet.h"
-#include "Lesson10_Platform.h"
-#include "Lesson11_ParallaxScrolling.h"
-
-#include "LessonAI01_SteeringBehaviors.h"
-#include "LessonAI02_NPCWave.h"
-
 #include "GamePlayScreen.h"
 #include "FoodDropGame.h"
 
